ec282c567ff523f5a25e48c22c0631f9e70c736a
John Lombardo <john@lombardos.org>
2012-07-10 16:18:15 -0700

    mrkn-envsetup.sh now calculates the FROM and TO directories on its own

--- bin/mrkn-envsetup.sh
+++ bin/mrkn-envsetup.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+DUPDEVDIR=device/marakana/alpha
+DUPSRCFIL=build/envsetup.sh
+
+# calculate DUPTO and DUPFROM
+if [ ! -d $DUPDEVDIR ]; then
+	echo >&2 "no $DUPDEVDIR. You're not in the right directory."
+	return 1
+fi
+unset DUPTO
+for DUPTEST in $(find . -mindepth 1 -maxdepth 1 -type d -printf "%P\n"); do
+	if [ -f $DUPTEST/$DUPSRCFIL ]; then
+		if [ ! -u $DUPTO ]; then
+			echo >&2 "More than one android directory. Fail."
+			return 1
+		fi
+		DUPTO=$PWD/$DUPTEST
+	fi
+done
+unset DUPTEST
+DUPFROM=$PWD/device
 
 function dup(){ 
-	part=${PWD:${#FROM}}
-	mkdir -p $TO/device/$part && cp $1 $TO/device/$part
+	part=${PWD:${#DUPFROM}}
+	mkdir -p $DUPTO/device/$part && cp $1 $DUPTO/device/$part
 }
+
+unset DUPDEVDIR
+unset DUPSRCFIL
